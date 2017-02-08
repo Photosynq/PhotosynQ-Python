488f2520ec2da953b6f6dd1c55a0d93d1b745286
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-02-08 15:27:47 -0500

    removed 'getpass' req from setup.py, incremented version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ['requests', 'getpass','json','pandas', 'datetime'],
+    install_requires = ['requests', 'json','pandas', 'datetime'],
 
     #package_data = {
     #    # If any package contains *.txt or *.rst files, include them:
