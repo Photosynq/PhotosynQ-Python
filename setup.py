4f6f9c0cd193bc0d1ec2ed9853b9522a1de3a789
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-02-08 15:28:00 -0500

    removed 'getpass' req from setup.py, incremented version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if sys.version_info < (3,0):
 from setuptools import setup, find_packages
 setup(
     name = "photosynq_py",
-    version = "0.8",
+    version = "0.9",
     packages = ['photosynq_py'],
     test_suite = 'tests',
 
