08b24cc3c15553ff4a8d86d74d9bd7e3a9556695
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-02-09 13:54:02 -0500

    incremented version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if sys.version_info < (3,0):
 from setuptools import setup, find_packages
 setup(
     name = "photosynq_py",
-    version = "0.11",
+    version = "0.12",
     packages = ['photosynq_py'],
     test_suite = 'tests',
 
