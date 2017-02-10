16652905e372750a0649d847279731dd914c13e3
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-02-10 16:03:08 -0500

    NOT PASSSING, added asserts to compare csv to buildDataFrame() result

--- tests/buildDataFrame_test.py
+++ tests/buildDataFrame_test.py
@@ -9,6 +9,8 @@ def loadTestJson( file ):
         result_bytes = testinfo_file.read()
     return json.loads( result_bytes.decode('utf-8') )
         
+ignorableCsvHeaders = [ "Series", "Repeat" ]
+    
 class buildDataFrame_test(TestCase):
     def test_buildDataFrame(self):
         
@@ -16,9 +18,20 @@ class buildDataFrame_test(TestCase):
         resourceFolder = "tests/resources/"
         testInfo = loadTestJson( resourceFolder + "1224_info_from_api" )["project"]
         testData = loadTestJson( resourceFolder + "1224_data_from_api" )["data"]
-        expectedDataFrame = DataFrame.from_csv( resourceFolder + "1224_csv_from_website.csv" )
+        csvDataFrame = DataFrame.from_csv( resourceFolder + "1224_csv_from_website.csv" )
 
         # build a dataframe 
         builtDataFrame = ps.buildProjectDataFrame( testInfo, testData )
         
         # compare the built dataframe to the one loaded from csv resource
+        builtDataKeys = builtDataFrame['Leaf Photosynthesis MultispeQ V1.0'].keys()
+        for csvColumnHeader in csvDataFrame.columns:
+            if csvColumnHeader in ignorableCsvHeaders:
+                continue;
+            self.assertIn( csvColumnHeader, builtDataKeys, "buildProjectDataFrame() result is missing header \"{0}\", which is present in test resources csv".format( csvColumnHeader ) )
+            
+            csvColumnData =  list(csvDataFrame[csvColumnHeader])
+            builtColumnData = builtDataFrame['Leaf Photosynthesis MultispeQ V1.0'][csvColumnHeader]
+            self.assertListEqual( csvColumnData, builtColumnData, "buildProjectDataFrame() result \"{0}\" values do not match the corresponding column in the test resources csv".format( csvColumnHeader ) ) 
+           
+            
